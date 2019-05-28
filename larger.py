5a51c803b9d512127f37ed6397b8a4958b649eed
sakthiganesan <50898232+sakthiganesan@users.noreply.github.com>
2019-05-28 19:32:23 +0530

    Create larger.py

---
+++ larger.py
@@ -0,0 +1,18 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=int(input("enter c value"))
+if(a>b):
+  if(a>c):
+    print(a)
+  else:
+    print(c)
+elif(b>a):
+  if(b>c):
+    print(b)
+  else:
+    print(c)
+elif(c>a):
+  if(c>b):
+      print(c)
+else:
+  print("invalid input")
