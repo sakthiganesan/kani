8cf285dcec942e8b081aad3a7864dfac7441db83
sakthiganesan <50898232+sakthiganesan@users.noreply.github.com>
2019-05-30 20:31:48 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,5 +1,17 @@
-c=int(input(""))
-for i in range(1,6):
-  sum=c*i
-  print(sum)
+C=int(input(""))
+L=[]
+a=1*C
+L.append(a)
+b=2*C
+L.append(b)
+c=3*C
+L.append(c)
+d=4*C
+L.append(d)
+e=5*C
+L.append(e)
+print(*L)
+
+  
+  
   
