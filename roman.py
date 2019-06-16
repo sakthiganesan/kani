1b6ea5355150d7812b27002f969e0dabea5f2df9
sakthiganesan <50898232+sakthiganesan@users.noreply.github.com>
2019-06-16 22:38:12 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,5 +1,5 @@
 NUM=(input())
-KHI=["zero","I","II","III","IV","V","VI","VII","VIII","IX","X"]
+KHI=["zero","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV"]
 for i in range(8):
   if NUM==KHI[i]:
     print(i)
