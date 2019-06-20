e01311780ac79e36330ffeade0104a662129172a
sakthiganesan <50898232+sakthiganesan@users.noreply.github.com>
2019-06-20 19:36:44 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -1,9 +1,9 @@
 a,b=input().split()
-add=0
+count=0
 for j in range(len(a)):
-  if a.add(a[j])==b.add(b[j]):
-    add=+1
-  if add==len(a):
+  if a.count(a[j])==b.count(b[j]):
+    count=+1
+  if count==len(a):
     print("yes")
   else:
     print("no")
